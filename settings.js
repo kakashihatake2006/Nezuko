94a21813e6948167d6dd59a35fddff369f7130fd
Kakashi hatake <105074780+kakashuuu@users.noreply.github.com>
2024-03-13 12:12:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "mongodb+srv://Redo:Redo@cluster0.iwmez.mongodb.net/myFirstDatabase?retryWrites=true";
 global.prefa = process.env.PREFIX || "+";
 global.ggle = process.env.GOOGLE_API || "AIzaSyDiPos9JPXolt0UBUSbrMSAAJYZLug5gIQ";
-global.wter = process.env.WEATHER_API || "none";
+global.wter = process.env.WEATHER_API || "85e001bf8619486d80f63636241303";
 global.sessionId = process.env.SESSION_ID || "TEST";
 global.malid = process.env.MAL_USERNAME || "Kakashu2006";
 global.malpass = process.env.MAL_PASSWORD || "R02ZJXBBWA";
